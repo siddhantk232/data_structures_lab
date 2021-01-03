8962429a50a805e72eff22ee9d3b95c6dd81a8c1
siddhantk232 <siddhantk232@gmail.com>
2021-01-03 19:19:23 +0530

    feat: linked_list
    WAP to insert an element in the beginning of a linear linked-list.
    WAP that removes an element from the beginning of a linear linked-list.
    WAP that removes an element from the end of a linear linked-list.

---
+++ linked_list.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// compiled using:
+// gcc (GCC) 10.2.0
+// 3. WAP to insert an element in the beginning of a linear linked-list.
+// 4. WAP that removes an element from the beginning of a linear linked-list.
+// 5. WAP that removes an element from the end of a linear linked-list.
+
+// linked list node 
+struct Node { 
+    int data; 
+    struct Node* next; 
+};
+
+void insert_element_at_the_beginning(struct Node** ll, int element);
+struct Node* remove_element_from_beginning(struct Node* ll);
+struct Node* remove_element_from_end(struct Node* ll);
+
+void printList(struct Node* ll) {
+  while (ll != NULL) {
+    printf("%d ", ll->data);
+    ll = ll->next;
+  }
+  printf("\n");
+}
+
+int main() {
+  struct Node* head = malloc(sizeof(struct Node));
+  head->data = 3;
+
+  // 3 
+
+  insert_element_at_the_beginning(&head, 5);
+  insert_element_at_the_beginning(&head, 9);
+  insert_element_at_the_beginning(&head, 12);
+
+  // 12 -> 9 -> 5 -> 3
+  head = remove_element_from_beginning(head);
+  // 9 -> 5 -> 3
+  
+  head = remove_element_from_end(head);
+
+  // 9 -> 5 
+  printList(head);
+}
+
+void insert_element_at_the_beginning(struct Node** ll, int element) {
+  // create a new node, with the provided element
+  struct Node* head = malloc(sizeof(struct Node));
+
+  head->data = element;
+  head->next = *ll;
+
+  // make new head
+  *ll = head;
+}
+
+struct Node* remove_element_from_beginning(struct Node* ll) {
+  struct Node *temp = ll;
+
+  // move head to next
+  ll = ll->next;
+
+  free(temp);
+
+  return ll;
+}
+
+struct Node* remove_element_from_end(struct Node* ll) {
+  // return NULL is ll is empty of length of ll is 1
+  if (ll == NULL || ll->next == NULL)
+    return NULL;
+
+  struct Node* second_last  = ll;
+
+  while(second_last->next->next != NULL) 
+    second_last = second_last->next;
+    
+  free(second_last->next);
+  second_last->next = NULL;
+
+  return ll;
+}
